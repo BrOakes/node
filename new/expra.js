fe8205ef023ccbb3838101a98c5eab70af4cd5e7
Bradley Oakes <BrOakesGames@gmail.com>
2018-08-16 00:54:55 -0400

    someotherstuffandhtml

--- new/expra.js
+++ new/expra.js
@@ -2,13 +2,25 @@ let express = require('express');
 let app = express();
 let path = require('path');
 
+let port = 8080;
+
 app.use('/rss', express.static(__dirname + "/rss"));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/rss/index.html'));
+    res.sendFile(path.join(__dirname + '/rss/html/index.html'));
 });
 
-app.listen(8081);
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname+'/rss/html/about.html'))
+})
+
+app.get('/other', function(req,res){
+    res.sendFile(path.join(__dirname+'/rss/html/other.html'))
+})
+
+app.listen(port);
+
+console.log(`Running on Port ${port}`);
 
 //I fuckin did it!!!
 
