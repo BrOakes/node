e6c473f88ee24ad41c2bbc0725a629d1654ccb9e
Bradley Oakes <BrOakesGames@gmail.com>
2018-08-17 00:11:03 -0400

    thebirdswontfly

---
+++ new/birds.js
@@ -0,0 +1,27 @@
+let express = require('express')
+let app = express();
+let router = express.Router()
+
+router.use(function timeLog(req, res, next){
+    console.log('Time: ', Date.now())
+    next()
+})
+router.get('/birds', function(req, res){
+    res.send('Birds home page')
+})
+
+router.get('/is', function(req, res){
+    res.send('About birds')
+})
+
+// app.use('/', router);
+
+// app.listen(8080,function(){
+//     console.log(`Running on Port 8080`);
+// });
+
+
+
+module.exports = router
+
+console.log(module.exports);
--- new/expra.js
+++ new/expra.js
@@ -1,10 +1,16 @@
 let express = require('express');
 let app = express();
 let path = require('path');
+let bodyParser = require('body-parser');
 
 let port = 8080;
 
+let birds = require('./birds');
+
+app.use('/brids', birds);
+
 app.use('/rss', express.static(__dirname + "/rss"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/rss/html/index.html'));
@@ -18,9 +24,49 @@ app.get('/other', function(req,res){
     res.sendFile(path.join(__dirname+'/rss/html/other.html'))
 })
 
-app.listen(port);
+app.get('/example/a', function (req, res) {
+    res.send('Hello from A!')
+  })
+app.get('/example/b', function (req, res, next) {
+    console.log('the response will be sent by the next function ...')
+    next()
+}, function (req, res) {
+res.send('Hello from B!')
+})
 
-console.log(`Running on Port ${port}`);
+let cb0 = function(req, res, next){
+    console.log('CB0');
+    next();
+}
+let cb1 = function(req, res, next){
+    console.log('CB1');
+    next();
+}
+let cb2 = function(req, res, next){
+    res.send('Hello from C!')
+}
+
+app.get('/example/c',[cb0,cb1,cb2]);
+
+//GET method route
+app.get('/t', function (req, res, next) {
+    res.send('GET request to the homepage')
+})
+
+// POST method route
+app.post('/other', function (req, res) {
+    res.end('POST request to the homepage')
+})
+
+app.all('/secret', function (req, res, next) {
+    console.log('Accessing the secret section ...')
+    next() // pass control to the next handler
+})
+
+app.listen(port,function(){
+    console.log(`Running on Port ${port}`);
+});
 
 //I fuckin did it!!!
+
 
